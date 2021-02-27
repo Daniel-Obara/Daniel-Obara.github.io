88a2974289eb6d5e27dc6f6a2fb7f35de2839d5e
Daniel-Obara <79760916+Daniel-Obara@users.noreply.github.com>
2021-02-27 12:22:22 -0500

    Add files via upload

--- App.java
+++ App.java
@@ -1,47 +1,108 @@
-
+import java.io.File;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-import java.util.List;
-
-
+/**
+ * App
+ */
 public class App {
 
-   public static void main(String[] args) {
+    public static String groomName()
+    {
+        String groomName = "";
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please Enter the Groom's Name: " );
+        groomName = scan.nextLine();
+        return groomName;
+    }
+    public static String brideName()
+    {
+        String brideName = "";
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please Enter the Bride's Name: " );
+        brideName = scan.nextLine();
+        return brideName;
+    }
+    public static int numbOfGuests()
+    {
+        int numbOfGuests = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please Enter the Number of Guests: " );
+        numbOfGuests = scan.nextInt();
+        return numbOfGuests;
+    }
+    public static double sqFt()
+    {
+        double sqFt = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please Enter the Square Footage of the room: " );
+        sqFt = scan.nextInt();
+        return sqFt;
+    }
+    public static ArrayList songNames()
+    {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<String> songs = new ArrayList<>();
+        String songRec = "";
+        String response = "";
+        boolean done = false;
+    while(done == false)
+    {
+System.out.println("What song would you like to listen to?");
+songRec = scan.nextLine();
+songs.add(songRec);
+System.out.println("are you done?");
+response = scan.nextLine();
+if(response.equalsIgnoreCase("yes"))
+{
+done = true;
+System.out.println("Song Request Completed");
+}
 
-   
+}
+        return songs;
 
-   List<Address> address = new ArrayList<Address>();
+        }
+    
+        public static double guestPerSqFt(int numbOfGuests, double sqFt)
+        {
+            double guestPerSqFt = 0;
+            guestPerSqFt = numbOfGuests/sqFt;
+            return guestPerSqFt;
+        }
+    
+    public static void main(String[] args) throws Exception {
+       String groomName = groomName();
+       String brideName = brideName();
+       int numbOfGuests = numbOfGuests();
+       double sqFt = sqFt();
+      ArrayList<String> songs = songNames();
+       double guestPerSqFt = guestPerSqFt(numbOfGuests, sqFt);
 
-   address.add(new Address("340 east beaver ave", "State College", "Pennsylvania", 16801, AddressType.LOCAL));
+       try
+       {
+           PrintWriter pw = new PrintWriter("assignment_1_wedding.txt");
+           pw.println(groomName);
+           pw.println(brideName);
+           pw.println(numbOfGuests);
+           pw.println(sqFt);
+          pw.println(songs);
+           pw.println(guestPerSqFt);
+           pw.close();
 
-   address.add(new Address("218 Spruce Run Dr.", "Dingmans Ferry", "Pennslyvania", 18328, AddressType.PERMANENT));
+        Scanner fileScan = new Scanner(new File("assignment_1_wedding.txt"));
+        while(fileScan.hasNextLine());
+        {
+            System.out.println(fileScan.nextLine());
+        }
+       
+       }
 
-   
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
 
-   Person p1 = new Student("Daniel", "Obara", 19, "State College", address, "101", "ETI", 3.02);
-
-   
-
-   List<Address> address1 = new ArrayList<Address>();
-
-   address1.add(new Address("612 Stuart Hall", "State College", "Pennsylvania", 16802, AddressType.LOCAL));
-
-   address1.add(new Address("101 street", "North Aleghany", "Pennsylvania", 101001, AddressType.PERMANENT));
-
-   
-
-   Person p2 = new StudentAthlete("Amir", "Zisling", 20, "State College", address1, "102", "Education", 4.0, "Flag Football");
-
-   
-
-   System.out.println("Person 1 details: ");
-
-   System.out.println(p1.toString());
-
-   System.out.println("\nPerson 2 details: ");
-
-   System.out.println(p2.toString());
-
-   }
-
-   }
+    }
+}
